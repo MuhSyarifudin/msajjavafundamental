2feac3e0e3a89efe2f4e2c4f351f8aee997e9715
Syarifudin <suicideudin@gmail.com>
2023-01-04 20:03:35 +0700

    tambahkan file buat commit

---
+++ src/main/java/com/juaracoding/msajjavafundamental/BuatCommit.java
@@ -0,0 +1,7 @@
+package com.juaracoding.msajjavafundamental;
+
+public class BuatCommit {
+    public static void main(String[] args) {
+        System.out.println("buat commit ke github");
+    }
+}
