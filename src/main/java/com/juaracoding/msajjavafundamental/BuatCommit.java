6331fde89f8f1e38c8876cef6bf3c4df81d69caf
Syarifudin <suicideudin@gmail.com>
2023-01-04 20:26:16 +0700

    tulis ulang file buat commit

--- src/main/java/com/juaracoding/msajjavafundamental/BuatCommit.java
+++ src/main/java/com/juaracoding/msajjavafundamental/BuatCommit.java
@@ -1,7 +1,14 @@
 package com.juaracoding.msajjavafundamental;
+/*
+Created By IntelliJ IDEA 2022.2.3 (Community Edition)
+Build #IU-222.4345.14, built on October 5, 2022
+@Author Syarifudin a.k.a. Paul Christian
+Java Developer
+Created on 04/01/2023 20:25
+@Last Modified 04/01/2023 20:25
+Version 1.1
+*/
+
 
 public class BuatCommit {
-    public static void main(String[] args) {
-        System.out.println("buat commit ke github");
-    }
 }
