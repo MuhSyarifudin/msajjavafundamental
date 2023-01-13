3197a87a63a1610f80d25ecc67947fec6c788b7e
Syarifudin <suicideudin@gmail.com>
2023-01-13 19:03:29 +0700

    tambah package testarray.

--- src/main/java/com/juaracoding/msajjavafundamental/testharian/testarray/DeklarasiArray.java
+++ src/main/java/com/juaracoding/msajjavafundamental/testharian/testarray/DeklarasiArray.java
@@ -1,4 +1,4 @@
-package com.juaracoding.msajjavafundamental.testharian;
+package com.juaracoding.msajjavafundamental.testharian.testarray;
 /*
 Created By IntelliJ IDEA 2022.3.1 (Community Edition)
 Build #IC-223.8214.52, built on December 20, 2022
