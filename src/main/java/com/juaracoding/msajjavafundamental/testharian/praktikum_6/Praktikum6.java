f4437a0b724f2c602443ac166baf3539dc779270
Syarifudin <suicideudin@gmail.com>
2023-02-06 14:18:21 +0700

    Upload Tugas Praktikum 6 soal nomor 2.

---
+++ src/main/java/com/juaracoding/msajjavafundamental/testharian/praktikum_6/Praktikum6.java
@@ -0,0 +1,49 @@
+package com.juaracoding.msajjavafundamental.testharian.praktikum_6;/*
+Created By IntelliJ IDEA 2022.3.2 (Ultimate Edition)
+Build #IU-223.8617.56, built on January 26, 2023
+@Author Syarifudin a.k.a. Muhamad Syarifuidn
+Java Developer
+Created on 06/02/2023 12:43
+@Last Modified 06/02/2023 12:43
+Version 1.1
+*/
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Praktikum6 {
+    /*
+	Soal Nomor 2 Praktikum 6 Java Fundamental
+    */
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> intArr = new ArrayList<Integer>();
+        int intValue;
+        int i = 0;
+        int j = 0;
+        int intFind = 0;
+        Boolean isFound = true;
+
+        System.out.println("Masukan angka sepuasnya : ");
+        do {
+            try{
+            intValue = sc.nextInt();
+            intArr.add(intValue);
+            }catch (Exception e){
+                j=-1;
+            }
+        }while(j!=-1);
+        System.out.println("Angka yang anda inputkan adalah : "+intArr);
+        System.out.print("Angka yang dicari adalah : ");
+        intFind = sc.nextInt();
+        for (int k=0;k<=intArr.size();k++){
+            if (intFind==intArr.get(k)){
+                System.out.println("Angka "+intFind+" ditemukan pada index ke "+i+"urutan ke "+i+1+" dari data yang di input.");
+            }
+        }
+        if (isFound){
+            System.out.println("Angka "+intFind+" yang anda cari tidak ditemukan!");
+        }
+    }
+}
